2e6391d12dc044397025cdc95843bda4c45b299f
jeong-han-kim <kimjh9813@naver.com>
2023-03-12 06:22:53 +0900

    FEAT : Add pcltopcl2 code

--- carrier_ros_perception/include/carrier_ros_perception/test.h
+++ carrier_ros_perception/include/carrier_ros_perception/test.h
@@ -2,6 +2,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_ros/point_cloud.h>
 
 class Pcl_to_pcl2
 {
@@ -24,18 +26,126 @@ class Pcl_to_pcl2
 
         // ROS Service Client
 
+        // ROS Timer
+        ros::Timer timer;
 
-        int lidar_numbeer;
+        int sensor_number;
+        int queue_size_;
+        std::string points_in_, pcl2_out_;
+        sensor_msgs::PointCloud2 pcl2_data1, pcl2_data2, pcl2_data3, pcl2_integrate;
 
     public:
         Pcl_to_pcl2();
         ~Pcl_to_pcl2();
+
+
+        inline std::string getFieldsList (const sensor_msgs::PointCloud &points);
+
+        void cloud_cb_points2 (const sensor_msgs::PointCloud2ConstPtr &msg);
+        void cloud_cb_points (const sensor_msgs::PointCloudConstPtr &msg);
 
         void initPublisher(void);
         void initSubscriber(void);
       
         void Sensor1Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg);
         void Sensor2Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg);
-        void Sensor3Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg);  
+        void Sensor3Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg); 
+
+        void TimerPclIntegrate(const ros::TimerEvent& event);
+
+};
+
+
+class PointCloudConverter 
+{
+  private:
+    // ROS
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_points_, sub_points2_;
+    ros::Publisher pub_points_, pub_points2_;
+
+    int queue_size_;
+    std::string points_in_, points2_in_, points_out_, points2_out_;
+
+  public:
+    PointCloudConverter () : nh_ ("~"), queue_size_ (100), 
+                             points_in_ ("/points_in"), points2_in_ ("/points2_in"), 
+                             points_out_ ("/points_out"), points2_out_ ("/points2_out")
+    {
+      // Subscribe to the cloud topic using both the old message format and the new
+      sub_points_ = nh_.subscribe (points_in_, queue_size_, &PointCloudConverter::cloud_cb_points, this);
+      sub_points2_ = nh_.subscribe (points2_in_, queue_size_, &PointCloudConverter::cloud_cb_points2, this);
+      pub_points_ = nh_.advertise<sensor_msgs::PointCloud> (points_out_, queue_size_);
+      pub_points2_ = nh_.advertise<sensor_msgs::PointCloud2> (points2_out_, queue_size_);
+      ROS_INFO ("PointCloudConverter initialized to transform from PointCloud (%s) to PointCloud2 (%s).", nh_.resolveName (points_in_).c_str (), nh_.resolveName (points2_out_).c_str ());
+      ROS_INFO ("PointCloudConverter initialized to transform from PointCloud2 (%s) to PointCloud (%s).", nh_.resolveName (points2_in_).c_str (), nh_.resolveName (points_out_).c_str ());
+    }
+    
+
+    inline std::string
+      getFieldsList (const sensor_msgs::PointCloud2 &cloud)
+    {
+      std::string result;
+      for (size_t i = 0; i < cloud.fields.size () - 1; ++i)
+        result += cloud.fields[i].name + " ";
+      result += cloud.fields[cloud.fields.size () - 1].name;
+      return (result);
+    }
+
+
+    inline std::string
+      getFieldsList (const sensor_msgs::PointCloud &points)
+    {
+      std::string result = "x y z";
+      for (size_t i = 0; i < points.channels.size (); i++)
+        result = result + " " + points.channels[i].name;
+      return (result);
+    }
+
+
+    void
+      cloud_cb_points2 (const sensor_msgs::PointCloud2ConstPtr &msg)
+    {
+      if (pub_points_.getNumSubscribers () <= 0)
+      {
+        //ROS_DEBUG ("[point_cloud_converter] Got a PointCloud2 with %d points on %s, but no subscribers.", msg->width * msg->height, nh_.resolveName (points2_in_).c_str ());
+        return;
+      }
+
+      ROS_DEBUG ("[point_cloud_converter] Got a PointCloud2 with %d points (%s) on %s.", msg->width * msg->height, getFieldsList (*msg).c_str (), nh_.resolveName (points2_in_).c_str ());
+      
+      sensor_msgs::PointCloud output;
+      // Convert to the new PointCloud format
+      if (!sensor_msgs::convertPointCloud2ToPointCloud (*msg, output))
+      {
+        ROS_ERROR ("[point_cloud_converter] Conversion from sensor_msgs::PointCloud2 to sensor_msgs::PointCloud failed!");
+        return;
+      }
+      ROS_DEBUG ("[point_cloud_converter] Publishing a PointCloud with %d points on %s.", (int)output.points.size (), nh_.resolveName (points_out_).c_str ());
+      pub_points_.publish (output);
+    }
+
+
+    void
+      cloud_cb_points (const sensor_msgs::PointCloudConstPtr &msg)
+    {
+      if (pub_points2_.getNumSubscribers () <= 0)
+      {
+        //ROS_DEBUG ("[point_cloud_converter] Got a PointCloud with %d points on %s, but no subscribers.", (int)msg->points.size (), nh_.resolveName (points_in_).c_str ());
+        return;
+      }
+
+      ROS_DEBUG ("[point_cloud_converter] Got a PointCloud with %d points (%s) on %s.", (int)msg->points.size (), getFieldsList (*msg).c_str (), nh_.resolveName (points_in_).c_str ());
+
+      sensor_msgs::PointCloud2 output;
+      // Convert to the old point cloud format
+      if (!sensor_msgs::convertPointCloudToPointCloud2 (*msg, output))
+      {
+        ROS_ERROR ("[point_cloud_converter] Conversion from sensor_msgs::PointCloud to sensor_msgs::PointCloud2 failed!");
+        return;
+      }
+      ROS_DEBUG ("[point_cloud_converter] Publishing a PointCloud2 with %d points on %s.", output.height * output.width, nh_.resolveName (points2_out_).c_str ());
+      pub_points2_.publish (output);
+    }
 
 };
--- carrier_ros_perception/src/test.cpp
+++ carrier_ros_perception/src/test.cpp
@@ -1,22 +1,66 @@
 #include "carrier_ros_perception/test.h"
 
+Pcl_to_pcl2::Pcl_to_pcl2()
+  :nh(""),
+  priv_nh("~"),
+  queue_size_ (100), 
+  pcl_in_ ("/pcl_in_1"),
+  pcl2_out_ ("/pcl2_out")
+  timer = nh.createTimer(ros::Duration(0.02), &Pcl_to_pcl2::TimerPclIntegrate, this); //50hz
+{
+
+  // priv_nh_.getParam("/* param_name */", /* param_name */);
+  // nh.param("wheel_left_joint_name", joint_states_name_[LEFT],  std::string("wheel_left_joint"));
+  sensor_number = nh.param<int>("sensor_number", 1);
+  // std::string yaml_file = nh.param<std::string>("dynamixel_info", "");
+  ROS_INFO ("PointCloudConverter initialized to transform from PointCloud (%s) to PointCloud2 (%s).", nh.resolveName (pcl_in_).c_str (), nh.resolveName (pcl2_out_).c_str ());
+}
+
+
+inline std::string Pcl_to_pcl2::getFieldsList (const sensor_msgs::PointCloud &points)
+{
+  std::string result = "x y z";
+  for (size_t i = 0; i < points.channels.size (); i++)
+    result = result + " " + points.channels[i].name;
+  return (result);
+}
+
+
+void Pcl_to_pcl2::Sensor1Callback (const sensor_msgs::PointCloudConstPtr &msg)
+{
+  if (pcl2_pub.getNumSubscribers () <= 0)
+  {
+    //ROS_DEBUG ("[point_cloud_converter] Got a PointCloud with %d points on %s, but no subscribers.", (int)msg->points.size (), nh.resolveName (pcl_in_).c_str ());
+    return;
+  }
+
+  ROS_DEBUG ("[point_cloud_converter] Got a PointCloud with %d points (%s) on %s.", (int)msg->points.size (), getFieldsList (*msg).c_str (), nh.resolveName (pcl_in_).c_str ());
+
+  // Convert to the old point cloud format
+  if (!sensor_msgs::convertPointCloudToPointCloud2 (*msg, pcl2_data1))
+  {
+    ROS_ERROR ("[point_cloud_converter] Conversion from sensor_msgs::PointCloud to sensor_msgs::PointCloud2 failed!");
+    return;
+  }
+  ROS_DEBUG ("[point_cloud_converter] Publishing a PointCloud2 with %d points on %s.", pcl2_data1.height * pcl2_data1.width, nh.resolveName (pcl2_out_).c_str ());
+  // pcl2_pub.publish (pcl2_data1);
+}
+
 void Pcl_to_pcl2::initPublisher()
 {
-  floor_pcl2_pub = nh.advertise<sensor_msgs::PointCloud2>("downsampled_point_cloud", 10);
+  pcl2_pub = nh.advertise<sensor_msgs::PointCloud2>(pcl2_out_, 100);
 }
 
 void Pcl_to_pcl2::initSubscriber()
 {
-  if(lidar_numbeer)
+  if(sensor_number)
   sensor1_sub = priv_nh.subscribe("pointcloud1", 100, &Pcl_to_pcl2::Sensor1Callback, this);
-  sensor2_sub = priv_nh.subscribe("pointcloud2", 100, &Pcl_to_pcl2::Sensor2Callback, this)
-  sensor3_sub = priv_nh.subscribe("pointcloud3", 100, &Pcl_to_pcl2::Sensor3Callback, this)
+  sensor2_sub = priv_nh.subscribe("pointcloud2", 100, &Pcl_to_pcl2::Sensor2Callback, this);
+  sensor3_sub = priv_nh.subscribe("pointcloud3", 100, &Pcl_to_pcl2::Sensor3Callback, this);
 }
 
-void Pcl_to_pcl2::Sensor1Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg)
-{
-  //
-}
+
+
 
 void Pcl_to_pcl2::Sensor2Callback(const sensor_msgs::PointCloudConstPtr &cloudmsg)
 {
@@ -27,7 +71,11 @@ void Pcl_to_pcl2::Sensor3Callback(const sensor_msgs::PointCloudConstPtr &cloudms
   //
 }
 
-
+void Pcl_to_pcl2::TimerPclIntegrate(const ros::TimerEvent& event)
+{
+  //pcl2_data1,pcl2_data2,pcl2_data3data를 가지고 합쳐서 내보내야함.
+  pcl2_pub.publish(pcl2_integrate);
+}
 
 int main(int argc, char** argv) 
 {
