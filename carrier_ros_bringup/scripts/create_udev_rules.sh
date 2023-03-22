00b9fcab9b1d70e1bc3ac60be1f608d4a3e8efaf
jeong-han-kim <kimjh9813@naver.com>
2023-03-22 18:24:45 +0900

    Add motor udev rule

--- carrier_ros_bringup/scripts/create_udev_rules.sh
+++ carrier_ros_bringup/scripts/create_udev_rules.sh
@@ -4,14 +4,14 @@ echo ""
 echo "This script copies Carrier_ros udev rules to /etc/udev/rules.d/"
 echo ""
 
-echo ""
-echo "Carrier_ros IMU (USB Serial) : /dev/ttyUSBx to /dev/ttyIMU :"
-if [ -f "/etc/udev/rules.d/Carrier_ros_imu.rules" ]; then
-    echo "Carrier_ros_imu.rules file already exist."
-else 
-    echo 'SUBSYSTEM=="tty*", ATTRS{idVendor}=="10c4" ATTRS{idProduct}=="ea60", ATTRS{serial}=="0001", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyIMU" ' > /etc/udev/rules.d/Carrier_ros_imu.rules
 
-    echo 'Carrier_ros_imu.rules created'
+echo "Carrier_ros motor (USB Serial) : /dev/ttyUSBx to /dev/ttyMotor :"
+if [ -f "/etc/udev/rules.d/Carrier_ros_motor.rules" ]; then
+    echo "Carrier_ros_motor.rules file already exist."
+else 
+    echo  'KERNEL=="tty*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", ATTRS{serial}=="0001", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyMotor"' >/etc/udev/rules.d/Carrier_ros_motor.rules    
+
+    echo 'Carrier_ros_lidar.rules created'
 fi
 
 echo ""
@@ -33,15 +33,16 @@ else
     echo 'Carrier_ros_lidar.rules created'
 fi
 
-# echo ""
-# echo "Carrier_ros dynamixel (USB Serial) : /dev/ttyUSBx to /dev/ttyDYNAMIXEL :"
-# if [ -f "/etc/udev/rules.d/Carrier_ros_dynamixel.rules" ]; then
-#     echo "Carrier_ros_dynamixel.rules file already exist."
-# else 
-#     echo  'KERNEL=="tty*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", ATTRS{serial}=="FT78LEKI", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyDYNAMIXEL"' >/etc/udev/rules.d/Carrier_ros_dynamixel.rules    
+echo ""
+echo "Carrier_ros IMU (USB Serial) : /dev/ttyUSBx to /dev/ttyIMU :"
+if [ -f "/etc/udev/rules.d/Carrier_ros_imu.rules" ]; then
+    echo "Carrier_ros_imu.rules file already exist."
+else 
+    echo 'SUBSYSTEM=="tty*", ATTRS{idVendor}=="10c4" ATTRS{idProduct}=="ea60", ATTRS{serial}=="0004", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyIMU" ' > /etc/udev/rules.d/Carrier_ros_imu.rules
 
-#     echo 'Carrier_ros_dynamixel.rules created'
-# fi
+    echo 'Carrier_ros_imu.rules created'
+fi
+
 
 # echo ""
 # echo "Carrier_ros arduino (USB Serial) : /dev/ttyACMx to /dev/ttyARDUINO :"
