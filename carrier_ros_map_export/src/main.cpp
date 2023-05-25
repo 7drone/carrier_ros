bea87a497fb0d214dfd12059a1a2276cc526b70c
jeong-han-kim <kimjh9813@naver.com>
2023-05-25 21:43:30 +0900

    Add tuning map

--- carrier_ros_map_export/src/main.cpp
+++ carrier_ros_map_export/src/main.cpp
@@ -99,7 +99,7 @@ bool MapServer::loadMapFromValues(std::string map_file_name, double resolution,
   // To make sure get a consistent time in simulation
   modified_map.info.map_load_time =ros::Time::now();
   map_resp_.map.info.map_load_time =ros::Time::now();
-  map_resp_.map.header.frame_id = "map";
+  map_resp_.map.header.frame_id = "utm";
   modified_map.header.frame_id = map_frame_id_;
   modified_map.header.stamp = ros::Time::now();
   map_resp_.map.header.stamp = ros::Time::now();
